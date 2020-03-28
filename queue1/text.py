89f72543b53bb7188b647622c1f367de8014a7ff
Administrator <956111647@qq.com>
2020-03-29 04:43:28 +0800

    jianzhi2

--- queue1/text.py
+++ queue1/text.py
@@ -1,8 +1,15 @@
 import queue
-a = queue.SimpleQueue()
-a.put(1)
-a.put(2)
-a.put(4)
-print(a.get())
-print(a.get())
-print(a.get())
+from collections import deque
+# a = queue.SimpleQueue()
+# a.put(1)
+# a.put(2)
+# a.put(4)
+# print(a.get())
+# print(a.get())
+# print(a.get())
+
+#双向队列
+#读取3个以后，读取第四个，第一个就会被顶出去
+q = deque(open('text.txt','r',encoding='utf-8'),3)
+print(list(q))
+
